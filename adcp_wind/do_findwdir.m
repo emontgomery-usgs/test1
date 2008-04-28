d9f3c3b5221c37e57f5bf9df5bca1023d287966b
emontgomery <emontgomery@c224c343-115a-4682-bd2c-f6853ee5f17b>
2008-04-28 15:41:46 +0000

    smart indented files and added a couple of comments.

--- adcp_wind/do_findwdir.m
+++ adcp_wind/do_findwdir.m
@@ -1,17 +1,22 @@
+% script do_findwdir.m
+%  DO_FINDWDIR:  script to run findwdir and produce some plots
+
 % script to remind myself how I did the ADCP wind dir stuff.
 %   etm 2/7/08
 % load a sample file (adcp2ep run on raw *wh000.cdf data)
+%  using an already processed file doesn't get the same result, even if the
+%  surface bins are lef in
 ncload('7751whall2.cdf')
-  tt=time+time2/86400000;
+tt=time+time2/86400000;
 % compute wind speed and direction from raw ADCP
 [dd,ss,okidx]=findwdir(u_1205,v_1206);
-   % used gregorian to find that tt(804)=10/1/04 and (3780)=11/1/04 
-   octindx=find(okidx>=804 & okidx < 3780);
-  
+% used gregorian to find that tt(804)=10/1/04 and (3780)=11/1/04
+octindx=find(okidx>=804 & okidx < 3780);
+
 %load noaa buoy wind data to compare
 ncload ('44013_89t05-cal.cdf')
-  ttwind=time+time2/86400000;
-  % used gregorian to find october 04 data  
+ttwind=time+time2/86400000;
+% used gregorian to find october 04 data
 % Now plot direction
 plot(tt(okidx(octindx)),dd(octindx),'.')
 hold on
--- adcp_wind/findwdir.m
+++ adcp_wind/findwdir.m
@@ -1,4 +1,4 @@
-function [dirsmax,spd_est,gt12]=findwdir(u,v)  
+function [dirsmax,spd_est,gt12]=findwdir(u,v)
 % FINDWDIR: compute wind direction from surface ADCP bin
 %   follows Marmorino and Hallock 2000 JAOT method
 %   The direction estimate correllates well with local wind measurements
@@ -10,29 +10,29 @@ function [dirsmax,spd_est,gt12]=findwdir(u,v)
 %     diratsmaz=findwdir(u_1205,v_1206)
 
 % first get the direction and spped
-  [spd,vdir]=pcoord(u,v); 
+[spd,vdir]=pcoord(u,v);
 %The direction & speed returned by pcoord are (5180x16)
 
 %Then we want to find the indices of maximum speed :
-  [val,indx]=max(spd');  % have to transpose to get indices right
-   %Then we want to use only those near the surface 
-   % this condition won't work in all cases
-   gt12=find(indx > 12);
-   % for speed, find the speed in the bin below max(speed)
-   indxw=indx(gt12)-1;
-   nspd=spd(gt12,indxw);
+[val,indx]=max(spd');  % have to transpose to get indices right
+%Then we want to use only those near the surface
+% this condition won't work in all cases
+gt12=find(indx > 12);
+% for speed, find the speed in the bin below max(speed)
+indxw=indx(gt12)-1;
+nspd=spd(gt12,indxw);
 
 % the direction result is good, the speed estimate is not
-  % this should be doable in one step, but I can't figure out how:
-  aa=(vdir(gt12,indx(gt12)));  
-  % what we want is on the diagonal of aa & nspd, so use an identity
-    ebb=eye(size(aa));
-     new=ebb.*aa;
-     nn=find(new>0);
-      dirsmax=new(nn);
-       spd2=nspd(nn);
-       spd_est=val(gt12)'-spd2;
-      
-  %    plot(gt12,dirsmax,'x')
-      
-      
+% this should be doable in one step, but I can't figure out how:
+aa=(vdir(gt12,indx(gt12)));
+% what we want is on the diagonal of aa & nspd, so use an identity
+ebb=eye(size(aa));
+new=ebb.*aa;
+nn=find(new>0);
+dirsmax=new(nn);
+spd2=nspd(nn);
+spd_est=val(gt12)'-spd2;
+
+%    plot(gt12,dirsmax,'x')
+
+
