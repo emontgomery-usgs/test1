5dd4130f572ff3b8417e9038e2352dacb27745b7
emontgomery <emontgomery@c224c343-115a-4682-bd2c-f6853ee5f17b>
2008-05-13 18:35:47 +0000

    These all work.  I kept the fix_lon a separate step to run on a whole directory, whild fix_buoy_ts should operate only on any files that need it.

---
+++ netcdf_stuff/do_EPIC_cvt.m
@@ -0,0 +1,18 @@
+%script do_EPIC_cvt.m
+% DO_EPIC_CVT: convert all the .cdf files in a directory to EPIC
+%
+% makes repeated calls to fix_buoy_ts.m to convert to EPIC
+
+% find the .cdf files in cwd
+fil=dir;
+    for ik=1:length(fil)-2
+        iscdf=~isempty(strfind(fil(ik+2).name, '.cdf'));
+        if  iscdf
+            files{ik}=fil(ik+2).name;
+        end
+    end
+    % run fix_buoy_ts on them
+    for jj=1:length(files)
+        nd=fix_buoy_ts(files{jj});
+    end
+
---
+++ netcdf_stuff/fix_poslon.m
@@ -0,0 +1,96 @@
+function [lon_info, posidx]=find_poslon(url_dir)
+% FIND_POSLON  reads metadata and returns list of files wi. positive longitude
+%
+% usage :
+%   [lon_info, posidx]=dir_to_ge(url_dir,chng_var_names);
+% outputs :
+%   lon_info is a structure containing the name, sign of longitude, and
+%      whether the lon attribute has the correct sign
+%   posidx is the list of indices where lon is positive (and needs changing)
+% input arguments:
+%   urldir is URL to read from (as below)
+%     urldir='http://stellwagen.er.usgs.gov/cgi-bin/nph-dods/DATAFILES/PV_SHELF/';
+%     use urldir='.'; to treat files in a local directory
+
+kk=0;k=0;j=0;jk=0;  % initialize bad file counters
+posidx=[];
+
+if nargin == 0
+    help mfilename; return
+end
+
+% selects *nc or *cdf if looking in cwd
+if strcmp(url_dir,'.')
+    fil=dir(url_dir);
+    for ik=1:length(fil)-2
+        isnc=~isempty(strfind(fil(ik+2).name, '.nc'));
+        iscdf=~isempty(strfind(fil(ik+2).name, '.cdf'));
+        if isnc || iscdf
+            files{ik}=fil(ik+2).name;
+        end
+    end
+else  % if not, all files have to be .nc or .cdf
+    [files]=get_files_from_url(url_dir);
+end
+
+% Loop through each NetCDF file, getting metadata, and collecting
+% urls where the metadata fails or lon is empty.
+
+% loop through everything
+% for i=1:10        % use this to do a subset of the data when testing.
+if isempty(files)
+    disp ('no files were found, try another location')
+    lon_info=[]; return
+else
+    for i=1:length(files);
+        % for i=1:10
+        file=char(files{i});
+        url=[url_dir '/' file ];
+        %
+        % open the file and get what you need
+        nc=netcdf(url, 'write');
+        lon=nc{'lon'}(1);
+        lonmeta=nc.longitude(:);
+
+        % see what lon is and fill the structure appropriately
+        if ~isempty(lon),
+            if lon >= 0
+                % see if if the metadata is correct and use that
+                if lonmeta == -lon
+                    j=j+1;
+                    lon_info(j)=struct('name',url,'sign','pos','metaOK',1);
+                    jk=jk+1; posidx(jk)=j;
+                    nc{'lon'}(1)=-lon;
+                    hist=nc.history;
+                    nh=['corrected sign of lon using ', mfilename, '.m: ' hist];
+                    nc.history = ncchar(nh);
+                    nc.CREATION_DATE=ncchar(datestr(now));
+                else
+                    j=j+1;
+                    lon_info(j)=struct('name',url,'sign','pos','metaOK',0);
+                    jk=jk+1; posidx(jk)=j;
+                    nc{'lon'}(1)=-lon;
+                    nc.longitude(:)=lon;
+                    hist=nc.history;
+                    nh=['corrected sign of lon using ', mfilename, '.m: '  hist];
+                    nc.history = ncchar(nh);
+                    nc.CREATION_DATE=ncchar(datestr(now));
+                end
+            else  % lon varible is negative but attribute is pos
+                if lonmeta == lon
+                    j=j+1;
+                    lon_info(j)=struct('name',url,'sign','neg','metaOK',1);
+                else
+                    j=j+1;
+                    lon_info(j)=struct('name',url,'sign','neg','metaOK',0);
+                    nc.longitude(:)=lon;
+                end
+            end
+            close (nc)
+        else
+            k=k+1;url_nolon{k}=url;
+        end
+    end
+end
+
+
